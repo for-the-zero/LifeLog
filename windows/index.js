f6084c11c88f732450c64f2c259e9fa14d9a661d
for-the-zero <ourmicroid@outlook.com>
2025-08-04 23:22:18 +0800

    windows developed

--- windows/index.js
+++ windows/index.js
@@ -2,13 +2,14 @@ const { app, Tray, Menu, nativeImage, dialog } = require('electron');
 const path = require('path');
 const cron = require('node-cron');
 const get_time = require('./db');
-const getActiveWindowInfo = require('./get-active');
+const { getActiveWindowInfo } = require('./get-active');
 
 const config = require('./config.json');
+const { get } = require('http');
 
 cron.schedule('5,15,25,35,45,55 * * * *', send_data);
 async function send_data(){
-    let active = getActiveWindowInfo();
+    let active = await getActiveWindowInfo();
     let time = await get_time(active.path);
     try{
         fetch(config.server_url + '/post', {
@@ -34,7 +35,7 @@ let tray = null;
 app.whenReady().then(() => {
     tray = new Tray(path.join(__dirname, 'icon.png'));
     const contextMenu = Menu.buildFromTemplate([
-        { label: '马上发送数据', type: 'normal', click: send_data },
+        { label: '马上发送数据', type: 'normal', click: ()=>{setTimeout(send_data, 500);} },
         { label: '退出', type: 'normal', click: () => app.quit() }
     ]);
     tray.setContextMenu(contextMenu);
