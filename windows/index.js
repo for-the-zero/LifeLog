c2768f56b6664df46aae8f967164108a875724e0
for-the-zero <ourmicroid@outlook.com>
2025-08-05 18:39:21 +0800

    [checkpoint]

--- windows/index.js
+++ windows/index.js
@@ -1,11 +1,17 @@
 const { app, Tray, Menu, nativeImage, dialog } = require('electron');
+const { spawn } = require('child_process');
 const path = require('path');
 const cron = require('node-cron');
+
 const get_time = require('./db');
 const { getActiveWindowInfo } = require('./get-active');
 
 const config = require('./config.json');
 const { get } = require('http');
+
+if(app.isPackaged){
+    app.setLoginItemSettings({ openAtLogin: true });
+};
 
 cron.schedule('5,15,25,35,45,55 * * * *', send_data);
 async function send_data(){
@@ -31,12 +37,38 @@ async function send_data(){
     };
 };
 
+function restart_as_admin() {
+    const elevatePath = path.join(process.resourcesPath, 'elevate.exe');
+    const exePath = process.execPath;
+    console.log('elevate.exe 路径:', elevatePath);
+    console.log('目标 EXE 路径:', exePath);
+    const child = spawn(elevatePath, [exePath], {
+        detached: true,
+        stdio: 'ignore',
+    });
+    child.unref();
+    app.quit();
+};
+
+var screenshot_mode = 1;
+const ss_mode_change = (mode) => {
+    screenshot_mode = mode;
+};
+
 let tray = null;
 app.whenReady().then(() => {
     tray = new Tray(path.join(__dirname, 'icon.png'));
     const contextMenu = Menu.buildFromTemplate([
+        { label: '截图模式', type: 'submenu', submenu: [
+            { label: '关闭', type: 'radio', checked: screenshot_mode === 0, click: () => { ss_mode_change(0) } },
+            { label: '按键检测', type: 'radio', checked: screenshot_mode === 1, click: () => { ss_mode_change(0) } },
+            { label: '剪贴板识别', type: 'radio', checked: screenshot_mode === 2, click: () => { ss_mode_change(0) } },
+        ] },
+        { type: 'separator'},
+
+        { label: '管理员身份重启', type: 'normal', click: () => { restart_as_admin() } },
         { label: '马上发送数据', type: 'normal', click: ()=>{setTimeout(send_data, 500);} },
-        { label: '退出', type: 'normal', click: () => app.quit() }
+        { label: '退出', type: 'normal', click: () => { app.quit() } }
     ]);
     tray.setContextMenu(contextMenu);
 });
